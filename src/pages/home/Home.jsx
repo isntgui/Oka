e1a2baa65c25238a6827d40dff85415cb4bb8edc
agui <souzaagnello70@gmail.com>
2026-09-02 18:05:19 -0400

    Fix CSS import casing

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { supabase } from "../../lib/supabase.js";
 import Navbar from "../../components/navbar.jsx";
-import "../../css/home/home.css";
+import "../../css/home/Home.css";
 
 export default function Home() {
     const navigate = useNavigate();
